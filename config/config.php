afc8b0624fbc4617ff6f41682172ffc9359116a3
fs2050 <fabiolfs76@hotmail.com>
2021-10-23 03:53:00 -0300

    deploy

--- config/config.php
+++ config/config.php
@@ -8,10 +8,10 @@ if(!defined('R4F5CC')){
 define("URLSITE", "https://localhost:8080/framework/");
 define('URLADM', "https://localhost:8080/framework/adm");
 
-define("HOST", 'localhost');
-define("USER", 'root');
-define("PASS", 'fox123');
-define("DBNAME", 'framework');
-define("PORT", 3308);
+define("HOST", 'ec2-18-211-243-247.compute-1.amazonaws.com');
+define("USER", 'udtvyekwrotmql');
+define("PASS", '58f93b638c9264a4446909487ef3c09b4b880a1d46eb4b84105c05ec42d8f1e4');
+define("DBNAME", 'd5s7pf4gm6mmru');
+define("PORT", 5432);
 
 define("EMAILADM", 'fabiolfs76@.com.br');
